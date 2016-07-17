b3d308f1020ffdc793613ce06c0a027d2ae5aaf6
Elva Chao <odekyc@Elvas-Air.lan>
2016-07-16 23:22:05 -0700

    hi

--- Heat_Map/es6/main.js
+++ Heat_Map/es6/main.js
@@ -2,7 +2,31 @@ import {tempData} from "./data";
 
 
 (function(){
-   alert(tempData["baseTemperature"]);
+   var margin = {top:20,right:20,bottom:20,left:25};
+   var data=tempData["monthlyVariance"];
+
+   var heatMapWidth=1085/263;
+   var heatMapHeight=400/12;
+
+   var benchTemp=8.66;
+   var yearFormat=d3.time.format('%Y');
+   var monthFormat=d3.time.format('%B');
+
+   var xAxisScale=d3.time.scale();
+
+   var xAxis=d3.svg.axis()
+               .orient("bottom")
+               .ticks(d3.time.years, 1)
+               .tickFormat(yearFormat);
+
+    var yAxisScale=d3.scale.linear()
+                   .range([0, 400])
+                   .domain([0,12]);
+    var yAxis=d3.svg.axis()
+                    .orient('left')
+                    .ticks(12)
+                    .tickFormat(monthFormat)
+                    .scale(yAxisScale);
 
   var colorCalibration = ['#4700b3','#00ace6','#5cd65c','#98e698','#d9ff66','#ffffcc','#ffe6b3', '#ffaa80', '#ff7733', '#ff3333','#990033'];
 
@@ -19,6 +43,9 @@ import {tempData} from "./data";
         return d;
       });
 
-
-
+     d3.select('#heatmap')
+       .append('g')
+       .attr('width', 1085 )
+       .attr('height', 400)
+       .attr('tansform', 'translate('+margin.left+','+margin.top+')');
 })();
